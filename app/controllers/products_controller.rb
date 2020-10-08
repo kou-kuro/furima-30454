544cfea2df8e4f934e5d8fa4b2a2f48fdb352dd0
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-10-08 14:21:04 +0900

    新規登録

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+  
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :fristname, presence: true
+  validates :lastname, presence: true
+  validates :firstname_kana, presence: true
+  validates :lastname_kana, presence: true
+  validates :birthday_id, presence: true
+  
+end
