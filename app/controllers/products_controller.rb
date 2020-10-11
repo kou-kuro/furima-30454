16f532a536f333bc3d684c0dc029eae1ccd31626
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-10-11 16:02:48 +0900

    jsの準備

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,13 @@ class ProductsController < ApplicationController
   end
 
   def create
-  end
+    @product = Product.create(product_params)
+    end
+
+ private
+ def product_params
+  params.require(:product).permit(:title, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :day_ship_id, :price, :image).merge(user_id: current_user.id)
+ end
 
 end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -11,13 +11,16 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :image, presence: true
 
-  
-  validates :number,numericality: { only_integer: true,greater_than: 300, less_than: 9,999,999
-  with_options presence: true, format: { with: /^[-]?[0-9]+(\.[0-9]+)?$/} do
-  validates :price
-
-  has_one_attached :image
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
 
+  
+  validates :price, numericality: {greater_than: 300, less_than: 9999999}
+  with_options presence: true, format: { with: /\A^[-]?[0-9]+(\.[0-9]+)?$\Z/} do
+  validates :price
+
+
+  end
 end
+
