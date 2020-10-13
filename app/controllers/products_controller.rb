136d0cd8c13ff74c649ba87bd442beb36140d409
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-10-13 17:43:27 +0900

    修正しました

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
-    @Product = Product.includes(:user).order('created_at DESC')
+    @product = Product.includes(:user).order('created_at DESC')
   end
 
   def new
