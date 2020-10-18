f39134f4e4f2ce1c96b4c551fbdce110699e7257
kou-kuro <71380718+kou-kuro@users.noreply.github.com>
2020-10-18 12:31:08 +0900

    修正１

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,9 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :move_to_root_path, only: [:index]
+  before_action :set_product, only: [:index, :create, :move_to_root_path]
 
   def index
-    @product = Product.find(params[:product_id])
     @payment = Payment.new
   end
 
@@ -11,16 +11,9 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @product = Product.find(params[:product_id])
     @payment = Payment.new(purchase_params)
     if @payment.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: @product.price,
-        card: purchase_params[:token],
-        currency: 'jpy'
-      )
-
+      pay_item
       @payment.save
       redirect_to root_path
     else
@@ -29,12 +22,24 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_root_path
-    @product = Product.find(params[:product_id])
     if user_signed_in? && current_user.id == @product.user_id
       redirect_to root_path
     elsif @product.purchase.present?
       redirect_to root_path
     end
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 
   private
